bb5f1af465adc6ebd720de9a58664345e8671444
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-10 22:00:08 +0200

    first draft of start/stop recording

--- main.js
+++ main.js
@@ -3,4 +3,34 @@
         alert('Not supported by your browser');
         throw new Error();
     }
+
+    var lastTranscript = "";
+    var recognition = new globalScope.webkitSpeechRecognition();
+
+    recognition.lang = "it-IT";
+
+    recognition.onstart = function () {
+        lastTranscript = "";
+    };
+
+    recognition.onresult = function(e){
+        _.each(e.results,function(result){
+            if(result.isFinal){
+                lastTranscript += _.maxBy(result, 'confidence').transcript;
+            }
+        });
+    };
+
+    recognition.onend = function(){
+        alert(lastTranscript);
+    };
+
+
+    globalScope.start = function(){
+        recognition.start();
+    };
+
+    globalScope.stop = function(){
+        recognition.stop();
+    };
 })(window);
